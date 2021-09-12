1bb9a8cb393fa93f944f2c768b77fe4adf3c9128
NagaVenkataSaiM <2000030558@kluniversity.in>
2021-09-12 21:18:51 +0530

    my os commit-1

---
+++ xv6-nvsos/clear.c
@@ -0,0 +1,9 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main()
+{
+printf(1,"\e[1;1H\e[2J\e[3J");
+exit();
+}
---
+++ xv6-nvsos/clear.d
@@ -0,0 +1,1 @@
+clear.o: clear.c /usr/include/stdc-predef.h types.h stat.h user.h
